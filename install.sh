c13ed4ad93ca9e2ed324466d2a7b811a487a5f26
dhilipsiva <dhilipsiva@gmail.com>
2014-04-25 11:41:47 +0530

    Install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#
+# install.sh
+# Copyright (C) 2014 dhilipsiva <dhilipsiva@gmail.com>
+#
+# Distributed under terms of the MIT license.
+#
+set -e
+echo "Make sure you have backed up all your existing dot files, if you had any."
+echo "Make sure you have added this machine's ssh public key to your GitHub acccount."
+mkdir -p ~/WIP
+rm -rf ~/WIP/dotfiles
+git clone --recursive git@github.com:dhilipsiva/dotfiles.git ~/WIP/dotfiles
+cd ~/WIP/dotfiles
+rsync --exclude ".git/" --exclude ".DS_Store" --exclude "install.sh" \
+	--exclude "README.md" --exclude "LICENSE.txt" -av --no-perms . ~
+source ~/.bash_profile
