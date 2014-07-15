8eb41bf0611d12923605038352dc90ac2fe76c5c
dhilipsiva <dhilipsiva@gmail.com>
2014-07-15 10:56:35 +0530

    Projects

--- install.sh
+++ install.sh
@@ -14,8 +14,8 @@ echo "Make sure you have backed up all your existing dot files, if you had any."
 echo "Make sure you have added this machine's ssh public key to your GitHub acccount."
 
 # I guess pretty much everything else is self-explantory
-mkdir -p ~/WIP
-rm -rf ~/WIP/dotfiles
-git clone --recursive git@github.com:dhilipsiva/dotfiles.git ~/WIP/dotfiles
-cd ~/WIP/dotfiles
+mkdir -p ~/Projects
+rm -rf ~/Projects/dotfiles
+git clone --recursive git@github.com:dhilipsiva/dotfiles.git ~/Projects/dotfiles
+cd ~/Projects/dotfiles
 ./copy.sh
