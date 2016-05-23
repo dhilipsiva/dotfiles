2f159cffa8ef4ed7c78aaac925a526456ee12539
dhilipsiva <dhilipsiva@gmail.com>
2016-05-23 14:30:34 +0530

    Updating dotfiles...

--- update.sh
+++ update.sh
@@ -9,31 +9,35 @@
 # Stop on first error
 set -e
 if [ "$(uname)" == "Darwin" ]; then
-    # Update brew stuff
-    {
-        brew update --all
-        brew upgrade --all
-        brew cleanup
-        brew prune
-        brew cask update
-        brew cask cleanup
-    } || {
-    echo "$BOLD $GREEN Brew is not installed $RESET"
+	# Update brew stuff
+	{
+		brew update --all
+		brew upgrade --all
+		brew cleanup
+		brew prune
+		brew cask update
+		brew cask cleanup
+	} || {
+	echo "$BOLD $GREEN Brew is not installed $RESET"
 }
 fi
 
+function gcommit() {
+	{
+		git add -A .
+		git commit -m "Updating dotfiles..."
+	} || {
+	echo "$BOLD $GREEN Noting to commit... $RESET"
+}
+}
+
 # Copy the required files to `dotfiles`
 cd ~/Projects/dotfiles
+gcommit
 cp -f ~/{.bash_profile,.gitignore_global,.gitconfig,.vimrc,Brewfile,Caskfile,aliases.sh,exports.sh,functions.sh,update.sh,.osx,.tmux.conf} .
 cp -f ~/.vim/templates/* .vim/templates/
 cp -f ~/.tmuxinator/* .tmuxinator/
-git add -A .
-
-{
-    git commit -m "Updating dotfiles..."
-} || {
-echo "$BOLD $GREEN Noting to commit... $RESET"
-}
+gcommit
 
 # Update the dependencies
 git pull origin master
@@ -41,20 +45,14 @@ git submodule init
 git submodule update
 git submodule foreach --recursive git checkout master
 git submodule foreach --recursive git pull
-git add -A .
-
-{
-    git commit -m "Updating dependencies..."
-} || {
-echo "$BOLD $GREEN Noting to commit... $RESET"
-}
+gcommit
 
 # Copy the updated stuff into home path
 ./copy.sh
 
 # Push the updated dotfiles
 {
-    git push origin master
+	git push origin master
 } || {
 echo "$BOLD $GREEN Sorry buddy, you dont have permisson fo push to my repo :( $RESET"
 }
