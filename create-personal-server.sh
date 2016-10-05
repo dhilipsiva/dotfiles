b53d3cfbf1cdcd2c4dd33163d633ec2ed8c69754
dhilipsiva <dhilipsiva@gmail.com>
2016-10-05 09:00:15 +0530

    prettify create script

--- create-personal-server.sh
+++ create-personal-server.sh
@@ -1,9 +1,39 @@
 export PERSONAL_SERVER_ZONE = "us-central1-c"
 
-gcloud compute --project $PERSONAL_CLOUD_PROJECT instances create $PERSONAL_SERVER_NAME --zone $PERSONAL_SERVER_ZONE --machine-type "f1-micro" --subnet "default" --private-network-ip "10.128.0.3" --address $PERSONAL_SERVER_IP --maintenance-policy "MIGRATE" --scopes default="https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly" --tags "http-server","https-server" --image "/debian-cloud/debian-8-jessie-v20160923" --boot-disk-size "20" --boot-disk-type "pd-standard" --boot-disk-device-name $PERSONAL_SERVER_NAME
+gcloud compute \
+	--project $PERSONAL_CLOUD_PROJECT \
+	instances create $PERSONAL_SERVER_NAME \
+	--zone $PERSONAL_SERVER_ZONE \
+	--machine-type "f1-micro" \
+	--subnet "default" \
+	--private-network-ip "10.128.0.3" \
+	--address $PERSONAL_SERVER_IP \
+	--maintenance-policy "MIGRATE" \
+   	--tags "http-server","https-server" \
+   	--image "/debian-cloud/debian-8-jessie-v20160923" \  # This needs to be constantly updated
+   	--boot-disk-size "20" \
+   	--boot-disk-type "pd-standard" \
+   	--boot-disk-device-name $PERSONAL_SERVER_NAME \
+	--scopes default="https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring.write","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly"
 
-gcloud compute --project $PERSONAL_CLOUD_PROJECT firewall-rules create "default-allow-http" --allow tcp:80 --network "default" --source-ranges "0.0.0.0/0" --target-tags "http-server"
 
-gcloud compute --project $PERSONAL_CLOUD_PROJECT firewall-rules create "default-allow-https" --allow tcp:443 --network "default" --source-ranges "0.0.0.0/0" --target-tags "https-server"
+gcloud compute \
+	--project $PERSONAL_CLOUD_PROJECT \
+	firewall-rules create "default-allow-http" \
+	--allow tcp:80 \
+	--network "default" \
+	--source-ranges "0.0.0.0/0" \
+	--target-tags "http-server"
 
-gcloud compute --project $PERSONAL_CLOUD_PROJECT ssh --zone $PERSONAL_SERVER_ZONE $PERSONAL_SERVER_NAME
+gcloud compute \
+	--project $PERSONAL_CLOUD_PROJECT \
+	firewall-rules create "default-allow-https" \
+	--allow tcp:443 \
+	--network "default" \
+	--source-ranges "0.0.0.0/0" \
+	--target-tags "https-server"
+
+gcloud compute \
+	--project $PERSONAL_CLOUD_PROJECT \
+	ssh \
+	--zone $PERSONAL_SERVER_ZONE $PERSONAL_SERVER_NAME
